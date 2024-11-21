00010fd8062bd831332b5328aa6eab0368049d58
Víctor Alfonso Mejía Morales <130195315+vamm99@users.noreply.github.com>
2024-11-21 09:04:11 -0500

    fix: remove unused appModule

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './AppModule';
+import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './lib/AllExceptionsFilter';
