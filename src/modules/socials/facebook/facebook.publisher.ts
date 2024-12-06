3b8cf99600683968a56663852234b9cab3f0044e
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2024-12-06 12:07:46 -0500

    facebook image

--- src/modules/socials/facebook/facebook.publisher.ts
+++ src/modules/socials/facebook/facebook.publisher.ts
@@ -16,7 +16,9 @@ export class FacebookPublisher implements PostPublisher {
       case 'message':
         await this.createMessagePost(data.accountId, data.token, fields);
         break;
-
+      case 'image':
+        await this.createPhotoPost(data.accountId, data.token, fields);
+        break;
       default:
         throw new Error('No se encontró el tipo de post');
     }
@@ -34,6 +36,33 @@ export class FacebookPublisher implements PostPublisher {
     }
     const url = `${this.graphUrl}${accountId}/feed`;
     const payload = {
+      message: fields.message,
+      access_token: token,
+    };
+
+    try {
+      await axios.post(url, payload);
+    } catch (error: any) {
+      throw new Error(
+        `Error al realizar la publicación: ${error.response?.data?.error?.message || error.message}`,
+      );
+    }
+  }
+
+  private async createPhotoPost(
+    accountId: string,
+    token: string,
+    fields: JsonValue,
+  ): Promise<void> {
+
+
+
+    if (typeof fields !== 'object' || !fields || !('url' in fields) || !('message' in fields)) {
+      throw new Error('El campo "url" es requerido en los datos de entrada.');
+    }
+    const url = `${this.graphUrl}${accountId}/photos`;
+    const payload = {
+      url: fields.url,
       message: fields.message,
       access_token: token,
     };
