2f0c81c61d42aed486b692a964e5882a69c9c00e
EmersonGuerrero1 <emersonguerrero.fr@academia.umb.edu.co>
2024-12-12 08:00:21 -0500

    change auth queries

--- src/modules/storage/storage.service.ts
+++ src/modules/storage/storage.service.ts
@@ -1,154 +1,182 @@
 import {
-  Injectable,
-  Inject,
-  NotFoundException,
-  InternalServerErrorException,
+	Injectable,
+	Inject,
+	NotFoundException,
+	InternalServerErrorException,
 } from '@nestjs/common';
 import { google, drive_v3 } from 'googleapis';
 import { PrismaClient } from '@prisma/client';
 import * as fs from 'fs';
 import * as path from 'path';
+import configLoader from '../../lib/ConfigLoader';
 
 @Injectable()
 export class StorageService {
-  private oauth2Client;
-  private drive: drive_v3.Drive;
-  private readonly prisma: PrismaClient;
+	private oauth2Client;
+	private drive: drive_v3.Drive;
+	private readonly prisma: PrismaClient;
 
-  constructor() {
-    this.oauth2Client = new google.auth.OAuth2(
-      process.env.GOOGLE_CLIENT_ID,
-      process.env.GOOGLE_CLIENT_SECRET,
-      process.env.GOOGLE_REDIRECT_URI,
-    );
+	constructor() {
+		this.oauth2Client = new google.auth.OAuth2(
+			configLoader().google.drive.clientId,
+			configLoader().google.drive.clientSecret,
+			configLoader().google.drive.redirectUri,
+		);
 
-    this.drive = google.drive({ version: 'v3', auth: this.oauth2Client });
-    this.prisma = new PrismaClient();
-  }
+		this.drive = google.drive({ version: 'v3', auth: this.oauth2Client });
+		this.prisma = new PrismaClient();
+	}
 
-  generateAuthUrl(): string {
-    return this.oauth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: ['https://www.googleapis.com/auth/drive'],
-    });
-  }
+	generateAuthUrl(profileId: number): string {
+		const baseUrl = this.oauth2Client.generateAuthUrl({
+			access_type: 'offline',
+			scope: ['https://www.googleapis.com/auth/drive'],
+		});
 
-  async setCredentials(profileId: number, service: string, code: string): Promise<void> {
-    try {
-      const { tokens } = await this.oauth2Client.getToken(code);
-      this.oauth2Client.setCredentials(tokens);
+		const authUrl = `${baseUrl}&profileId=${profileId}`;
+		return authUrl;
+	}
 
-      await this.saveToken(
-        profileId,
-        service,
-        tokens.access_token!,
-        tokens.refresh_token,
-        tokens.expiry_date ? new Date(tokens.expiry_date) : undefined,
-      );
-    } catch (error) {
-      throw new InternalServerErrorException('Error setting credentials: ' + error.message);
-    }
-  }
+	async setCredentials(
+		profileId: number,
+		service: string,
+		code: string,
+	): Promise<void> {
+		try {
+			const { tokens } = await this.oauth2Client.getToken(code);
+			this.oauth2Client.setCredentials(tokens);
 
-  async listFiles(profileId: number, service: string): Promise<drive_v3.Schema$File[]> {
-    try {
-  
-      await this.loadToken(profileId, service);
+			await this.saveToken(
+				profileId,
+				service,
+				tokens.access_token!,
+				tokens.refresh_token,
+				tokens.expiry_date ? new Date(tokens.expiry_date) : undefined,
+			);
+		} catch (error) {
+			throw new InternalServerErrorException(
+				'Error setting credentials: ' + error.message,
+			);
+		}
+	}
 
-      const res = await this.drive.files.list({
-        pageSize: 50,
-        fields: 'files(id, name, mimeType, webViewLink, thumbnailLink)',
-      });
+	async listFiles(
+		profileId: number,
+		service: string,
+	): Promise<drive_v3.Schema$File[]> {
+		try {
+			await this.loadToken(profileId, service);
 
-      return res.data.files || [];
-    } catch (error) {
-      throw new InternalServerErrorException('Error listing files: ' + error.message);
-    }
-  }
+			const res = await this.drive.files.list({
+				pageSize: 50,
+				fields: 'files(id, name, mimeType, webViewLink, thumbnailLink)',
+			});
 
-  async uploadFile(
-    profileId: number,
-    service: string,
-    filePath: string,
-    mimeType: string,
-  ): Promise<drive_v3.Schema$File | null> {
-    try {
-      await this.loadToken(profileId, service);
+			return res.data.files || [];
+		} catch (error) {
+			throw new InternalServerErrorException(
+				'Error listing files: ' + error.message,
+			);
+		}
+	}
 
-      const fileName = path.basename(filePath);
+	async uploadFile(
+		profileId: number,
+		service: string,
+		filePath: string,
+		mimeType: string,
+	): Promise<drive_v3.Schema$File | null> {
+		try {
+			await this.loadToken(profileId, service);
 
-      const res = await this.drive.files.create({
-        requestBody: {
-          name: fileName,
-        },
-        media: {
-          mimeType,
-          body: fs.createReadStream(filePath),
-        },
-      });
+			const fileName = path.basename(filePath);
 
-      return res.data;
-    } catch (error) {
-      throw new InternalServerErrorException('Error uploading file: ' + error.message);
-    }
-  }
+			const res = await this.drive.files.create({
+				requestBody: {
+					name: fileName,
+				},
+				media: {
+					mimeType,
+					body: fs.createReadStream(filePath),
+				},
+			});
 
-  private async saveToken(
-    profileId: number,
-    service: string,
-    accessToken: string,
-    refreshToken?: string,
-    expiryDate?: Date,
-  ): Promise<void> {
-    try {
-      const existingRecord = await this.prisma.storageProfile.findUnique({
-        where: { profileId_service: { profileId, service:"GOOGLE_DRIVE" } },
-      });
+			return res.data;
+		} catch (error) {
+			throw new InternalServerErrorException(
+				'Error uploading file: ' + error.message,
+			);
+		}
+	}
 
-      if (existingRecord) {
-        await this.prisma.storageProfile.update({
-          where: { id: existingRecord.id },
-          data: {
-            accessToken,
-            refreshToken,
-            expiryDate,
-            updatedAt: new Date(),
-          },
-        });
-      } else {
-        await this.prisma.storageProfile.create({
-          data: {
-            profileId,
-            service:'GOOGLE_DRIVE',
-            accessToken,
-            refreshToken,
-            expiryDate,
-          },
-        });
-      }
-    } catch (error) {
-      throw new InternalServerErrorException('Error saving token: ' + error.message);
-    }
-  }
+	private async saveToken(
+		profileId: number,
+		service: string,
+		accessToken: string,
+		refreshToken?: string,
+		expiryDate?: Date,
+	): Promise<void> {
+		try {
+			const existingRecord = await this.prisma.storageProfile.findUnique({
+				where: {
+					profileId_service: { profileId, service: 'GOOGLE_DRIVE' },
+				},
+			});
 
-  private async loadToken(profileId: number, service: string): Promise<void> {
-    try {
-      console.log(profileId)
-      const tokenRecord = await this.prisma.storageProfile.findUnique({
-        where: { profileId_service: { profileId:1, service:'GOOGLE_DRIVE' } },
-      });
+			if (existingRecord) {
+				await this.prisma.storageProfile.update({
+					where: { id: existingRecord.id },
+					data: {
+						accessToken,
+						refreshToken,
+						expiryDate,
+						updatedAt: new Date(),
+					},
+				});
+			} else {
+				await this.prisma.storageProfile.create({
+					data: {
+						profileId,
+						service: 'GOOGLE_DRIVE',
+						accessToken,
+						refreshToken,
+						expiryDate,
+					},
+				});
+			}
+		} catch (error) {
+			throw new InternalServerErrorException(
+				'Error saving token: ' + error.message,
+			);
+		}
+	}
 
-      if (!tokenRecord) {
-        throw new NotFoundException('Token not found for the given profile and service.');
-      }
+	private async loadToken(profileId: number, service: string): Promise<void> {
+		try {
+			const tokenRecord = await this.prisma.storageProfile.findUnique({
+				where: {
+					profileId_service: {
+						profileId: 1,
+						service: 'GOOGLE_DRIVE',
+					},
+				},
+			});
 
-      this.oauth2Client.setCredentials({
-        access_token: tokenRecord.accessToken,
-        refresh_token: tokenRecord.refreshToken,
-        expiry_date: tokenRecord.expiryDate?.getTime(),
-      });
-    } catch (error) {
-      throw new InternalServerErrorException('Error loading token: ' + error.message);
-    }
-  }
+			if (!tokenRecord) {
+				throw new NotFoundException(
+					'Token not found for the given profile and service.',
+				);
+			}
+
+			this.oauth2Client.setCredentials({
+				access_token: tokenRecord.accessToken,
+				refresh_token: tokenRecord.refreshToken,
+				expiry_date: tokenRecord.expiryDate?.getTime(),
+			});
+		} catch (error) {
+			throw new InternalServerErrorException(
+				'Error loading token: ' + error.message,
+			);
+		}
+	}
 }
