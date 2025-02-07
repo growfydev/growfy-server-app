39bc7f614e584c59206cce9696c2468dddcea33a
A. Mestra <89419093+uzideath@users.noreply.github.com>
2025-02-06 22:24:42 -0500

    update packages

--- src/core/prisma.service.ts
+++ src/core/prisma.service.ts
@@ -1,45 +1,16 @@
 import { PrismaClient } from '@prisma/client';
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import configLoader from 'src/lib/ConfigLoader';
-
-/**
- * PrismaService manages the connection to the database.
- * It should be injected into classes requiring database access.
- * For detailed Prisma documentation, visit: https://pris.ly/d/prisma-schema
- */
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-	constructor() {
-		const { database } = configLoader();
-
-		const datasourceUrl = PrismaService.constructDatasourceUrl(database);
-		super({
-			datasources: {
-				db: { url: datasourceUrl },
-			},
-		});
-	}
-
-	/**
-	 * Initializes the PrismaService by establishing the database connection.
-	 */
-	async onModuleInit(): Promise<void> {
+export class PrismaService
+	extends PrismaClient
+	implements OnModuleInit, OnModuleDestroy
+{
+	async onModuleInit() {
 		await this.$connect();
 	}
 
-	/**
-	 * Constructs the datasource URL for the database connection.
-	 * @param database - The database configuration object.
-	 * @returns The constructed datasource URL.
-	 */
-	private static constructDatasourceUrl(database: {
-		user: string;
-		password: string;
-		host: string;
-		port: number;
-		name: string;
-	}): string {
-		return `postgresql://${database.user}:${database.password}@${database.host}:${database.port}/${database.name}?schema=public`;
+	async onModuleDestroy() {
+		await this.$disconnect();
 	}
 }
